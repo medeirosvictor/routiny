5786506006bd21c6f69a3a5cf766bb926475962f
Victor Medeiros <victor.medeiros@osf.digital>
2024-03-25 11:57:10 -0400

    templates

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/medeirosvictor/routiny/templates"
 )
 
 func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello, World!")
+		component := templates.Home()
+		return component.Render(c.Request().Context(), c.Response().Writer)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
