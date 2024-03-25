d8c2c0584c324f5864b032882975ba0f313f7806
Victor Medeiros <victor.medeiros@osf.digital>
2024-03-25 12:11:40 -0400

    templates

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 	"github.com/medeirosvictor/routiny/templates"
 )
@@ -11,6 +13,18 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		component := templates.Home()
 		return component.Render(c.Request().Context(), c.Response().Writer)
+	})
+
+	e.GET("/add-task", func(c echo.Context) error {
+		task := c.QueryParam("task")
+		fmt.Println(task)
+		return c.String(200, "Task added")
+	})
+
+	e.GET("/remove-task", func(c echo.Context) error {
+		task := c.QueryParam("task")
+		fmt.Println(task)
+		return c.String(200, "Task removed")
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
