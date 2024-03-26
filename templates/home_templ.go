7e8ddaf075e28eec5d7f1ce4ddf606fb93353a10
Victor Medeiros <victor.medeiros@osf.digital>
2024-03-26 11:27:09 -0400

    templates

--- templates/home_templ.go
+++ templates/home_templ.go
@@ -35,7 +35,7 @@ func Home() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = components.addTaskForm().Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = components.AddTaskForm().Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
